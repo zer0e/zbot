c728360d99fd52f6b77603b20bc4b340e1eec9e8
ZclusLLoye <810464986@qq.com>
2020-11-15 23:30:34 +0800

    add new test plugin

--- src/main/java/com/github/zer0e/mbot/plugins/TestPlugin.java
+++ src/main/java/com/github/zer0e/mbot/plugins/TestPlugin.java
@@ -2,28 +2,50 @@ package com.github.zer0e.mbot.plugins;
 
 
 import com.github.zer0e.mbot.core.Api;
-import com.github.zer0e.mbot.model.GroupMsg;
-import com.github.zer0e.mbot.model.Msg;
+import com.github.zer0e.mbot.msg.FriendMsg;
+import com.github.zer0e.mbot.msg.GroupMsg;
+import com.github.zer0e.mbot.msg.TempMsg;
+import lombok.Getter;
 
-public class TestPlugin extends BasePlugin{
+// 需要getter
+@Getter
+public class TestPlugin implements GroupPlugin,FriendPlugin{
+    private Api api;
     public TestPlugin() {
         // 初始化
         init();
     }
 
     @Override
-    protected void init() {
+    public void init() {
         // 初始化关键字与监听的群组
-        this.description = "测试插件";
-        this.words.add("test");
-        this.groups.add("*");
+        this.group_words_set.add("test");
+        this.group_ids_set.add("*");
+
+        // 初始化私聊关键字和监听人id
+        this.friend_words_set.add("test");
+        this.friend_ids_set.add("*");
+
+        // 初始化API避免大量申请session
+        api = new Api();
     }
 
+    @Override
+    public int callback(FriendMsg msg) {
+        Api api = new Api();
+        boolean is_ok = api.send_plain_msg_to_friend(msg.getSender_id(), "收到test");
+        return is_ok ? 1 : 0;
+    }
 
     @Override
-    public int callback(Msg msg) {
-        Api api = new Api();
-        api.send_plain_msg_to_group(((GroupMsg) msg).getSender_group(), "收到test");
-        return 1;
+    public int callback(TempMsg msg) {
+        boolean is_ok = api.send_plain_msg_to_tmp_friend(msg.getSender_group_id(),msg.getSender_id(), "收到test");
+        return is_ok ? 1 : 0;
+    }
+
+    @Override
+    public int callback(GroupMsg msg) {
+        boolean is_ok = api.send_plain_msg_to_group(msg.getSender_group(), "收到test");
+        return is_ok ? 1 : 0;
     }
 }
