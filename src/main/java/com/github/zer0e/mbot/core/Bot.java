1148e85010a23471a41d56221ab93c618bdfc827
ZclusLLoye <810464986@qq.com>
2020-11-15 23:36:40 +0800

    when receiver or hanler stop, other thread will be stop

--- src/main/java/com/github/zer0e/mbot/core/Bot.java
+++ src/main/java/com/github/zer0e/mbot/core/Bot.java
@@ -2,7 +2,6 @@ package com.github.zer0e.mbot.core;
 
 import com.alibaba.fastjson.JSONObject;
 import lombok.Data;
-import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,14 +11,15 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 @Data
 public class Bot {
-    private Handle handle;
+    private Handler handler;
     private PluginLoader plugin_loader;
     private Receiver receiver;
     private Registry registry;
     private LinkedBlockingQueue<JSONObject> queue;
     private Api api;
-    private Thread handle_thread;
+    private Thread handler_thread;
     private Thread receiver_thread;
+    private Thread health_thread;
 
     private final static Logger logger = LoggerFactory.getLogger(Bot.class);
 
@@ -37,8 +37,8 @@ public class Bot {
         if (registry == null){
             registry = new Registry(plugin_loader);
         }
-        if (handle == null){
-            handle = new Handle(registry, queue);
+        if (handler == null){
+            handler = new Handler(registry, queue);
         }
         if (receiver == null){
             String session = api.get_session();
@@ -55,20 +55,14 @@ public class Bot {
     }
 
     public boolean start(){
-        if (handle == null || receiver == null || registry == null){
+        if (handler == null || receiver == null || registry == null){
             logger.error("启动失败，请先创建bot");
             return false;
         }
         try {
-            handle_thread = new Thread(new Runnable() {
+            handler_thread = new Thread(new Runnable() {
                 public void run() {
-                    try {
-                        handle.start();
-                    }catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }finally {
-                        logger.info("处理线程退出");
-                    }
+                    handler.start();
                 }
             });
             receiver_thread = new Thread(new Runnable() {
@@ -76,8 +70,16 @@ public class Bot {
                     receiver.start();
                 }
             });
-            handle_thread.start();
+            health_thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    health();
+                }
+            });
+
+            handler_thread.start();
             receiver_thread.start();
+            health_thread.start();
         }catch (Exception e){
             e.printStackTrace();
             return false;
@@ -85,14 +87,47 @@ public class Bot {
         return true;
     }
     public boolean stop(){
-        if (handle_thread == null || receiver_thread == null){
+        if (handler_thread == null || receiver_thread == null){
             logger.info("无需停止");
             return true;
         }
-        handle.setStop(true);
-        receiver.stop();
+        try {
+            handler.setStop(true);
+            handler_thread.interrupt();
+            receiver.stop();
+            handler_thread.join();
+            receiver_thread.join();
+        }catch (InterruptedException ignored){
+
+        }
+        logger.info("停止所有线程");
         return true;
     }
+
+    // 负责当receiver退出时，将所有线程结束
+    public void health(){
+        logger.info("健康线程启动");
+
+        try {
+            while (true){
+                if (receiver_thread == null || handler_thread == null){
+                    continue;
+                }
+                if (receiver.is_close){
+                    this.stop();
+                    break;
+                }
+                Thread.sleep(1);
+            }
+        }catch (InterruptedException ignored){
+
+        }finally {
+            logger.info("健康线程结束");
+        }
+
+
+    }
+
 
     public static void main(String[] args) {
         Bot bot = new Bot().build();
