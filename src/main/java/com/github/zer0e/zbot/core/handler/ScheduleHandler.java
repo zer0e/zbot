c1918cf8bfc3f0e07fd09493c914fd98245a1bd8
ZclusLLoye <810464986@qq.com>
2020-11-19 16:33:23 +0800

    add scheduleHandler

---
+++ src/main/java/com/github/zer0e/zbot/core/handler/ScheduleHandler.java
@@ -0,0 +1,68 @@
+package com.github.zer0e.zbot.core.handler;
+
+import com.github.zer0e.zbot.core.PluginLoader;
+import com.github.zer0e.zbot.core.Registry;
+import com.github.zer0e.zbot.utils.ReflectionUtils;
+import lombok.NonNull;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ScheduleHandler {
+    private Registry registry;
+    private Scheduler scheduler;
+    private volatile boolean stop = false;
+    private static Logger logger = LoggerFactory.getLogger(MsgHandler.class);
+
+
+    public ScheduleHandler(@NonNull Registry registry) {
+        this.registry = registry;
+    }
+
+    public void start(){
+        if (scheduler != null && !stop){
+            logger.info("定时任务已经启动");
+            return;
+        }
+        try {
+            scheduler = StdSchedulerFactory.getDefaultScheduler();
+//            Set<JobDetail> jobDetailSet = new HashSet<>();
+            // 创建job
+            for (Object o : registry.getScheduler_obj()){
+                // 多个cron表达式
+                for (String schedulerTime : (Set<String>)ReflectionUtils.getField(o,"schedulerTimeSet")){
+                    JobDetail jobDetail = JobBuilder.newJob((Class<? extends Job>) o.getClass())
+                            .build();
+                    CronTrigger cronTrigger = TriggerBuilder.newTrigger()
+                            .startNow()
+                            .withSchedule(CronScheduleBuilder.cronSchedule(schedulerTime))
+                            .build();
+                    scheduler.scheduleJob(jobDetail,cronTrigger);
+                }
+            }
+            scheduler.start();
+            stop = false;
+        }catch (SchedulerException e){
+            e.printStackTrace();
+            logger.error("quartz启动失败");
+            this.stop();
+        }
+    }
+
+    public void stop(){
+        if (scheduler == null && stop){
+            logger.info("无需停止");
+            return ;
+        }
+        try {
+            scheduler.shutdown();
+            stop = true;
+        }catch (Exception ignored) {
+
+        }
+    }
+}
