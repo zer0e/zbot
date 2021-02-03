277bf465d09c5466447a1072598be6578fbe8c8f
ZclusLLoye <810464986@qq.com>
2021-02-03 21:29:02 +0800

    add delete tmp dir function

--- src/main/java/com/github/zer0e/zbot/utils/FileUtil.java
+++ src/main/java/com/github/zer0e/zbot/utils/FileUtil.java
@@ -1,12 +1,18 @@
 package com.github.zer0e.zbot.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.UUID;
 
 public class FileUtil {
+    private static Logger logger = LoggerFactory.getLogger(FileUtil.class);
     public static String bytes_to_tmp_file(byte[] bytes){
-        return bytes_to_tmp_file(bytes, "tmp000");
+        String filename = UUID.randomUUID().toString();
+        return bytes_to_tmp_file(bytes, filename);
     }
     public static String bytes_to_tmp_file(byte[] bytes, String filename){
         String tmp_dir = get_tmp_dir();
@@ -20,6 +26,7 @@ public class FileUtil {
             file.createNewFile();
             fos = new FileOutputStream(file);
             fos.write(bytes);
+            logger.info("文件存放于： " + full_path);
         }catch (IOException e){
             e.printStackTrace();
             return null;
@@ -33,6 +40,25 @@ public class FileUtil {
             }
         }
         return full_path;
+    }
+    public static boolean delete_tmp_dir(){
+        String tmp_dir = get_tmp_dir();
+        File tmp_file_dir = new File(tmp_dir);
+        for (File file : tmp_file_dir.listFiles()){
+            if (file != null){
+                delete_tmp_file(file);
+            }
+        }
+        return tmp_file_dir.delete();
+    }
+    public static void delete_tmp_file(File file){
+        if (file.isFile()){
+            file.delete();
+        }else{
+            for (File tmp_file : file.listFiles()){
+                delete_tmp_file(tmp_file);
+            }
+        }
     }
 
     public static String get_project_dir(){
@@ -48,12 +74,10 @@ public class FileUtil {
         String os = get_os();
         String project_dir = get_project_dir();
         StringBuilder tmp_dir = new StringBuilder();
-        if (os.indexOf("windows") != 0){
+        if (os.contains("windows")){
             tmp_dir.append("\\tmp\\");
-        }else if (os.indexOf("linux") != 0){
-            tmp_dir.append("/tmp/");
         }else {
-            return "/tmp/";
+            tmp_dir.append("/tmp/");
         }
         return project_dir + tmp_dir;
     }
